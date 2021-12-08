3ef01c631721aa339072c2c05860a589bb09a2f1
Torres Martinez <adrian.torresmartinez@Fiserv.com>
2021-12-08 14:57:22 -0600

    first commit

---
+++ People.API/Controllers/PeopleController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using People.API.Data;
+using People.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace People.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        #region Constructor
+        private readonly IDataAccess _db;
+
+        public PeopleController(IDataAccess db)
+        {
+            this._db = db;
+        }
+
+        #endregion
+
+        // GET: api/<People>
+        [HttpGet]
+        public Task<List<PersonModel>> Get()
+        {
+            return _db.GetAllPeople();
+        }
+
+        // GET api/<People>/5
+        [HttpGet("{id}")]
+        public Task<PersonModel> Get(int id)
+        {
+            return _db.GetPeopleById(id);
+        }
+
+        // POST api/<People>
+        [HttpPost]
+        public Task<PersonModel> Post([FromBody] PersonModel data)
+        {
+            return _db.AddPeople(data);
+        }
+
+        // PUT api/<People>
+        [HttpPut]
+        public Task<PersonModel> Put([FromBody] PersonModel data)
+        {
+            return _db.UpdPeople(data);
+        }
+
+        // DELETE api/<People>/1
+        [HttpDelete("{id}")]
+        public Task Delete(int id)
+        {
+            return _db.DelPeople(id);
+        }
+
+    }
+}
---
+++ People.API/Data/DataAccess.cs
@@ -0,0 +1,85 @@
+﻿using People.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.API.Data
+{
+    public class DataAccess : IDataAccess
+    {
+        #region DataList
+
+        //TODO: Change to Real DB
+
+        private List<PersonModel> tbPeople = new List<PersonModel> {
+              new PersonModel { Id = 1, Name="Mario", LastName ="Gomez",Email="mariogomez56@gmail.com"},
+              new PersonModel { Id = 2, Name = "Jose", LastName = "Robles", Email = "joserobles09@gmail.com" }
+            };
+
+        #endregion
+
+        #region Methods
+
+        public Task<List<PersonModel>> GetAllPeople()
+        {
+            return Task.FromResult(tbPeople);
+        }
+
+        public Task<PersonModel> GetPeopleById(int id)
+        {
+            PeopleExist(id);
+
+            var person = tbPeople.Where(w => w.Id == id).FirstOrDefault();
+
+            return Task.FromResult(person);
+        }
+
+        public Task<PersonModel> AddPeople(PersonModel people)
+        {
+            // Get and set Id
+            var id = tbPeople.Max(s => s.Id) + 1;
+            people.Id = id;
+
+            tbPeople.Add(people);
+            return Task.FromResult(people);
+        }
+
+        public Task<PersonModel> UpdPeople(PersonModel people)
+        {
+            var Id = people.Id;
+
+            PeopleExist(Id);
+
+            var pIndex = tbPeople.FindIndex(w => w.Id == Id);
+            tbPeople[pIndex] = people;
+            return Task.FromResult(people);
+        }
+
+        public Task DelPeople(int Id)
+        {
+            PeopleExist(Id);
+
+            tbPeople.RemoveAll(x => x.Id == Id);
+            return Task.FromResult(true);
+        }
+
+        #endregion
+
+        #region Auxiliar
+
+        private void PeopleExist(int Id)
+        {
+            var pIndex = tbPeople.FindIndex(w => w.Id == Id);
+            if (pIndex == -1)
+            {
+                //TODO: log error here
+
+                throw new ArgumentException($"Not found people with id { Id}");
+            }
+        }
+
+        #endregion
+
+    }
+}
